45396b5b96c1a67efe4e4d5af99e1d9d944d08a2
andremcqu <mcquade.andre@gmail.com>
2022-05-10 16:08:09 +0200

    As I understood the logic, I added more Views

--- App.js
+++ App.js
@@ -3,9 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, }}>
+      <View style={{ flex: 2, flexDirection: "row" }}>
+        <View style={{ flex: 8, }}>
+          <View style={{ flex: 3, backgroundColor: "purple" }} />
+          <View style={{ flex: 1, backgroundColor: "yellow" }} />
+          <View style={{ flex: 1, backgroundColor: "pink" }} />
+        </View>
+        <View style={{ flex: 2, backgroundColor: "red" }} />
+        <View style={{ flex: 1, backgroundColor: "blue" }} />
+        <View style={{ flex: 2, }}>
+          <View style={{ flex: 3, backgroundColor: "turquoise" }} />
+          <View style={{ flex: 1, backgroundColor: "pink" }} />
+        </View>
+      </View>
+      <View style={{ flex: 2, }}>
+          <View style={{ flex: 1, backgroundColor: "gray" }} />
+        </View>
     </View>
   );
 }
